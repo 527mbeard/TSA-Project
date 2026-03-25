cdc94b7a6038042b0b916b3dad5fb611d3f7adee
Madison Beard <527mbeard@frhsd.com>
2026-03-25 03:00:53 +0000

    directory finished except title

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ const resources = [
     { name: "Aberdeen Easter Egg Hunt", category: "Family", location: "Aberdeen", link:"https://tourism.visitmonmouth.com/events/?imgoing-event=aberdeen-townships-annual-easter-egg-hunt-69b7695a5622fd44ce9ca75e" },
     { name: "Englishtown Farmers Market ", category: "Family", location: "June 20 - September 26 <br> (Borough Hall, 15 Main Street, Englishtown)", link:"https://englishtownauction.com/" },
     { name: "Freehold Open Door Non-Profit", category: "Family", location: "80 Throckmorton Street Freehold, NJ 07728", link: "https://www.faopendoor.org/" },
-    { name: "Tutoring Center", category: "Family", location: "Free tutoring for students." },
-    { name: "Tutoring Center", category: "Family", location: "Free tutoring for students." },
-    { name: "Tutoring Center", category: "Family", location: "Free tutoring for students." },
-    { name: "Tutoring Center", category: "Family", location: "Free tutoring for students." },
-    { name: "Tutoring Center", category: "Family", location: "Free tutoring for students." },
-    { name: "Tutoring Center", category: "Family", location: "Free tutoring for students." },
-    { name: "Tutoring Center", category: "Family", location: "Free tutoring for students." },
-    { name: "Tutoring Center", category: "Family", location: "Free tutoring for students." },
-    { name: "Tutoring Center", category: "Family", location: "Free tutoring for students." },
-    { name: "Tutoring Center", category: "Family", location: "Free tutoring for students." }
+    { name: "Spring Craft Show", category: "Shows", location: "March 28 <br> (2566 Guam Lane, Tinton Falls NJ 07724)", link:"https://www.monmouthcountyparks.com/page.aspx?ID=4206" },
+    { name: "Climb Time at Shark River Park", category: "Family", location: "April 10 <br> (1101 Schoolhouse Road, Wall, NJ 07753)", link:"https://www.monmouthcountyparks.com/page.aspx?Id=2537" },
+    { name: "WWE Trivia Night", category: "Trivia", location: "April 15 <br> (Woodbridge Brewing Co.)", link:"https://www.eventbrite.com/e/wwe-trivia-tickets-1981872968399?aff=ebdssbdestsearch" },
+    { name: "Friends Trivia Night", category: "Trivia", location: "June 24 <br> (Woodbridge Brewing Co.)", link:"https://www.eventbrite.com/e/friends-trivia-tickets-1985670246159?aff=ebdssbdestsearch" },
+    { name: "Candlelight: Coldplay vs. Imagine Dragons", category: "Shows", location: "April 3 <br> (Pollak Theatre, Monmouth University)", link:"https://feverup.com/m/385678"},
+    { name: "A Tribute to the best of the Beatles", category: "Shows", location: "May 21 <br> (Monmouth County Library Marlboro Branch)", link:"https://monmouthcountylib.libcal.com/event/16351170" }
 ];
 
 function displayResources(list) {
